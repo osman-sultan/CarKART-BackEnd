c9c95f6851e89cb40b805423eee58f2f4e66d717
Osman Sultan <o.sultan2811@gmail.com>
2022-10-26 18:07:54 -0400

    initial commit

---
+++ src/main/java/com/example/CarMarketplace/CarMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarMarketplace/CarMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
