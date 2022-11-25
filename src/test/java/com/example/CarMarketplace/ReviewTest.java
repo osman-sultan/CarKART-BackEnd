88b2cd8528ba1f9db408f5dbc8a67c2016986625
AntonMlynczyk <anton.mlynczyk@mail.utoronto.ca>
2022-11-25 12:05:25 -0500

    Create and add review testing

--- src/test/java/com/example/CarMarketplace/ReviewTest.java
+++ src/test/java/com/example/CarMarketplace/ReviewTest.java
@@ -1,6 +1,8 @@
 package com.example.CarMarketplace;
 
+import com.example.CarMarketplace.model.entity.Car;
 import com.example.CarMarketplace.model.entity.Company;
+import com.example.CarMarketplace.model.entity.Review;
 import com.example.CarMarketplace.model.repository.CompanyRepository;
 import com.example.CarMarketplace.model.repository.ReviewRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -45,11 +47,41 @@ class ReviewTest {
 		ObjectNode receivedJson = objectMapper.readValue(response.getContentAsString(), ObjectNode.class);
 		// Make sure all attributes are correct using .get()
 		assertEquals(1L, receivedJson.get("id").longValue());
-		assertEquals(1L, receivedJson.get("user").get("id"));
-		assertEquals(1L, receivedJson.get("car").get("id"));
-		assertEquals("09/27/2022", receivedJson.get("dateTimeStamp"));
-		assertEquals("Amazing car!", receivedJson.get("reviewText"));
+		assertEquals(1L, receivedJson.get("user").get("id").longValue());
+		assertEquals(1L, receivedJson.get("car").get("id").longValue());
+		assertEquals("09/27/2022", receivedJson.get("dateTimeStamp").textValue());
+		assertEquals("Amazing car!", receivedJson.get("reviewText").textValue());
 	}
 
+	@Test
+	void addReview() throws Exception{
+
+		// Create new JSON object containing necessary details
+		ObjectNode reviewJson = objectMapper.createObjectNode();
+		reviewJson.put("id", 12345L);
+		reviewJson.put("userId", 2L);
+		reviewJson.put("carId", 2L);
+		reviewJson.put("dateTimeStamp", "11/22/2022");
+		reviewJson.put("reviewText", "Nice");
+
+		// Send a POST request and retrieve the response
+		MockHttpServletResponse response = mockMvc.perform(
+						post("/reviews").
+								contentType("application/json").
+								content(reviewJson.toString()))
+				.andReturn().getResponse();
+
+		// assert HTTP code of response is 200 OK
+		assertEquals(200, response.getStatus());
+
+		assertTrue(reviewRepository.findById(12345L).isPresent());
+		Review addedReview = reviewRepository.findById(12345L).get();
+
+		assertEquals(12345L, addedReview.getId());
+		assertEquals(2L, addedReview.getCar().getId());
+		assertEquals(2L, addedReview.getCar().getId());
+		assertEquals("11/22/2022", addedReview.getDateTimeStamp());
+		assertEquals("Nice", addedReview.getReviewText());
+	}
 
 }
